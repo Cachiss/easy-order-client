d665a861d7fbe8a79bbe37948cae9f417c7ab4b4
cachis <marcoscachis@gmail.com>
2023-08-03 10:37:39 -0600

    finish restaurant card

--- src/components/restaurants/card_restaurant.jsx
+++ src/components/restaurants/card_restaurant.jsx
@@ -4,17 +4,17 @@ const Restaurant = ({data}) => {
 
   return (
     <div className="hero">
-      <div className="rounded flex flex-col justify-center items-center p-7" key={index}>
+      <div className="rounded flex flex-col justify-center items-center p-7 h-96" key={index}>
         <img src={image} alt={"Imagen de "+ name} className="w-2/3"/>
         <div className="flex flex-col w-full mt-3">
           <h3 className="text-4xl text-center">{name}</h3>
           <p className="font-Cavean text-2xl mt-2 ">{description}</p>
         </div>
-        <div className="flex flex-wrap mt-8">
+        <div className="flex flex-wrap mt-5 w-full justify-center items-end h-full">
           {/* List tags*/}
-          {tags.map((data)=>{
+          {tags.map((data,index)=>{
             return (
-              <div className="bg-orange rounded-lg text-lg px-1 h-auto mr-2">{data}</div>
+              <div className="bg-orange rounded-lg text-lg px-1 mr-2 h-6ll" key={index}>{data}</div>
               )
             })
           }
